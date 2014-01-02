738685a3c40c951760ab27a2a8151c6f7b1af5ee
sonnym <michaud.sonny@gmail.com>
2014-01-02 17:01:40 -0500

    iterate over files in index rather than manifest
    the manifest would not always be populated, whereas the index is always
    populated and consequently does not cause empty output

--- lib/js_asset_paths/generator.rb
+++ lib/js_asset_paths/generator.rb
@@ -9,13 +9,25 @@ module JsAssetPaths
     private
 
     def self.asset_hash
-      manifest.files.reduce({}) do |memo, (key, value)|
-        memo.merge(value['logical_path'] => (context.digest_assets? ? key : value['logical_path']))
+      assets.each_file.each_with_object({}) do |filepath, memo|
+        memo[filepath.relative_path_from(base_asset_path)] = output_path(filepath)
       end
     end
 
-    def self.manifest
-      Sprockets::Manifest.new(environment.assets.index, './public/assets')
+    def self.base_asset_path
+      Rails.root.join('app', 'assets')
+    end
+
+    def self.output_path(filepath)
+      if context.digest_assets?
+        "#{filepath.basename(filepath.extname)}-#{assets.file_digest(filepath)}#{filepath.extname}"
+      else
+        filepath.basename.to_s
+      end
+    end
+
+    def self.assets
+      environment.assets
     end
   end
 end
