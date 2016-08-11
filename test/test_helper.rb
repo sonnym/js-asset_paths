a3a02dd30322573411a71155e13c2d27f2584c76
Leon Miller-Out <leon@singlebrook.com>
2016-08-11 15:20:20 -0400

    Get test working.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,9 @@ require 'minitest/autorun'
 require 'pry'
 
 ENV['RAILS_ENV'] = 'test'
-#require File.expand_path('../dummy/config/environment.rb',  __FILE__)
-#require 'rails/test_help'
-require 'rails/all'
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
+require 'rails/test_help'
 
 require File.expand_path('../../lib/js_asset_paths.rb', __FILE__)
 
