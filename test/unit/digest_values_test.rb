7f5870efe90031fe9fcfd1dcc6513a4d01888ab5
Sonny Michaud <michaud.sonny@gmail.com>
2017-06-15 02:45:04 -0400

    test edge case values of asset digest

---
+++ test/unit/digest_values_test.rb
@@ -0,0 +1,39 @@
+require 'execjs'
+require_relative '../test_helper'
+
+describe 'untrue values of config.assets.digest' do
+  before do
+    App.initialize! unless App.initialized?
+  end
+
+  it 'output has corresponding key and value pairs when digest is false' do
+    begin
+      Rails.application.config.assets.digest = false
+
+      JsAssetPaths::Generator.environment = Rails.application
+      @output = JsAssetPaths::Generator.generate!
+
+      map = ExecJS.eval(@output)
+
+      map.each do |key, value|
+        file = key.split('/').second
+        assert_equal file, value
+      end
+
+    ensure
+      Rails.application.config.assets.digest = true
+    end
+  end
+
+  it 'output has corresponding key and value pairs when digest is not boolean' do
+    begin
+      Rails.application.config.assets.digest = :foo
+
+      JsAssetPaths::Generator.environment = Rails.application
+
+      assert_raises { JsAssetPaths::Generator.generate! }
+    ensure
+      Rails.application.config.assets.digest = true
+    end
+  end
+end
